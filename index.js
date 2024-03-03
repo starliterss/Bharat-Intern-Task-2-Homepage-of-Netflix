1ae2d0a3d562d7841e9d35cdefe67fcba286093c
Shreya Singh <120489161+starliterss@users.noreply.github.com>
2024-03-04 04:20:53 +0530

    Uploaded

---
+++ index.js
@@ -0,0 +1,21 @@
+// let accordions = document.getElementsByClassName("FAQ__title");
+
+// for (let i = 0; i < accordions.length; i++) {
+//   accordions[i].addEventListener("click", function () {
+//     let icon = this.querySelector("i");
+//     if (icon.classList.contains("fa-plus")) {
+//       icon.classList.remove("fa-plus");
+//       icon.classList.add("fa-times");
+//     } else {
+//       icon.classList.remove("fa-times");
+//       icon.classList.add("fa-plus");
+//     }
+
+//     let content = this.nextElementSibling;
+//     if (content.style.maxHeight) {
+//       content.style.maxHeight = null;
+//     } else {
+//       content.style.maxHeight = content.scrollHeight + "px";
+//     }
+//   });
+// }
